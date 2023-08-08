5769243ac7e621c3539b9b24b282ce96ea93f6dd
wtblock <44687173+wtblock@users.noreply.github.com>
2023-08-07 19:47:58 -0500

    Simple MFC command line template

--- CommandLineBug/CommandLineBug.cpp
+++ CommandLineBug/CommandLineBug.cpp
@@ -1,6 +1,6 @@
-// CommandLineBug.cpp : Defines the entry point for the console application.
-//
-
+/////////////////////////////////////////////////////////////////////////////
+// Copyright © 2023 by W. T. Block, all rights reserved
+/////////////////////////////////////////////////////////////////////////////
 #include "stdafx.h"
 #include "CommandLineBug.h"
 
@@ -8,39 +8,44 @@
 #define new DEBUG_NEW
 #endif
 
-
 // The one and only application object
-
 CWinApp theApp;
 
 using namespace std;
 
-int main()
+// a simple command line application to illustrate a bug in command line
+// processing
+int _tmain( int argc, TCHAR* argv[], TCHAR* envp[] )
 {
-    int nRetCode = 0;
+	HMODULE hModule = ::GetModuleHandle( NULL );
+	if ( hModule == NULL )
+	{
+		_tprintf( _T( "Fatal Error: GetModuleHandle failed\n" ) );
+		return 1;
+	}
 
-    HMODULE hModule = ::GetModuleHandle(nullptr);
+	// initialize MFC and error on failure
+	if ( !AfxWinInit( hModule, NULL, ::GetCommandLine(), 0 ) )
+	{
+		_tprintf( _T( "Fatal Error: MFC initialization failed\n " ) );
+		return 2;
+	}
 
-    if (hModule != nullptr)
-    {
-        // initialize MFC and print and error on failure
-        if (!AfxWinInit(hModule, nullptr, ::GetCommandLine(), 0))
-        {
-            // TODO: change error code to suit your needs
-            wprintf(L"Fatal Error: MFC initialization failed\n");
-            nRetCode = 1;
-        }
-        else
-        {
-            // TODO: code your application's behavior here.
-        }
-    }
-    else
-    {
-        // TODO: change error code to suit your needs
-        wprintf(L"Fatal Error: GetModuleHandle failed\n");
-        nRetCode = 1;
-    }
+	// write to console
+	CStdioFile fOut( stdout );
+	CString csMessage;
 
-    return nRetCode;
+	// display the number of arguments
+	fOut.WriteString( _T( ".\n" ) );
+	csMessage.Format( _T( "The number of parameters are %d\n.\n" ), argc );
+	fOut.WriteString( csMessage );
+
+	// display the arguments
+	for ( int i = 0; i < argc; i++ )
+	{
+		csMessage.Format( _T( "Parameter %d is %s\n.\n" ), i, argv[ i ] );
+		fOut.WriteString( csMessage );
+	}
+
+	return 0;
 }
